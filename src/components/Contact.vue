d054654e472a5291dfe388f18c7101a604152bc6
Michael Lobel <mglobel@gmail.com>
2017-02-21 17:37:45 -0600

    nabber cleanup and google maps embed

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -12,69 +12,23 @@
             C: 267-567-4966
           <p>
             E: RSimonLSW@gmail.com
+          <p>
+            1429 Walnut Street, Suite 1300
+          <p>
+            Philadelphia, PA 19102
         </section>
       </div>
       <section class="contact-details col-sm-6">
-        <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=-75.16659021377565%2C39.948953540235145%2C-75.16433715820314%2C39.95048335567248&amp;layer=mapnik&amp;marker=39.949718452230464%2C-75.16546368598938" style="border: 1px solid black"></iframe><br/><small><a href="http://www.openstreetmap.org/?mlat=39.94972&amp;mlon=-75.16546#map=19/39.94972/-75.16546">View Larger Map</a>
+        <iframe width="500" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=1429%20Walnut%20Street%2C%20Suite%201300%20Philadelphia%2C%20PA%2019102&key=AIzaSyDyQpzdh030Eoxm4tfZ3afFJaXPYO8lTKU" allowfullscreen></iframe>
       
-        <br />
-        1429 Walnut Street, Suite 1300
-        <br />
-        Philadelphia, PA 19102
+
       </section>
     </div>
   </section>
 </template>
 
 <script>
-  import loadGoogleMapsAPI from 'load-google-maps-api';
-  console.log(document.getElementsByClassName('maps'))
-
-  var x = document.getElementsByClassName("maps");
-  var i;
-  console.log(Array.from(x))
-  for (i = 0; i < x.length; i++) {
-      console.log(x[i])
-  }
- 
-  loadGoogleMapsAPI({key: 'AIzaSyDyQpzdh030Eoxm4tfZ3afFJaXPYO8lTKU'}).then((googleMaps) => {
-    var map = new googleMaps.Map(document.getElementsByClassName('maps'), {
-      zoom: 4,
-      center: {lat:39.949911, lng:-75.165409}
-    }); 
-    console.log(map)
-  }).catch((err) => {
-    console.error(err);
-  });
-
-
-  // function initMap() {
-  //   var uluru = {lat: -25.363, lng: 131.044};
-  //   var map = new google.maps.Map(document.getElementById('map'), {
-  //     zoom: 4,
-  //     center: uluru
-  //   });
-  //   var marker = new google.maps.Marker({
-  //     position: uluru,
-  //     map: map
-  //   });
-  // }
-
-  // console.log(Map)
-
-  // console.log(Map)
-  // load({key: 'AIzaSyDyQpzdh030Eoxm4tfZ3afFJaXPYO8lTKU', v: '3.25'})
-  // let MapDisplay = Map
-
-  // export default {
-  //   components: {
-  //     MapDisplay
-  //   }
-  // }
-
   export default {
-    components: {
-    }
   }
 </script>
 
@@ -83,5 +37,4 @@
   /* this should really just be a layout class */
   h1
     color: #42b983
-  section
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,13 @@
 <template>
   <nav class="navbar navbar-default">
-    <!-- rainbow here -->
-    <div>
-      <router-link class="navbar-brand" to="/">Rachel Simon, LSW MEd</router-link>
+
+
+    <div id="hamburgler" v-on:click="checkNav" class="hamburgler-icon-wrapper hidden-md hidden-lg">
+      <span class="hamburgler-icon"></span>
     </div>
-    <ul class="nav navbar-nav navbar-right">
+
+    <!-- rainbow here -->
+    <ul class="nav navbar-nav navbar-right hidden-xs hidden-sm">
       <li>
         <router-link to="/" class="nav-item nav-link"> Home </router-link>
       </li>
@@ -17,12 +20,136 @@
       <li>
         <router-link to="/contact" class="nav-item nav-link"> Contact </router-link>
       </li>
-      <li>&nbsp;&nbsp;&nbsp;&nbsp;</li>
     </ul>
   </nav>
 </template>
 
 <script>
+  // document.getElementById('hamburgler').addEventListener('click', checkNav);
+  // window.addEventListener("keyup", function(e) {
+  //   if (e.keyCode == 27) closeNav();
+  // }, false);
+
+  // function checkNav() {
+  //   if (document.body.classList.contains('hamburgler-active')) {
+  //     closeNav();
+  //   } else {
+  //     openNav();
+  //   }
+  // }
+
+  // function closeNav() {
+  //   document.body.classList.remove('hamburgler-active');
+  // }
+
+  // function openNav() {
+  //   document.body.classList.add('hamburgler-active');
+  // }
+
+  // new Vue({
+  //   el: '#hamburgler',
+  // })
+
   export default {
+    methods: {
+      checkNav: function (message) {
+        alert('Open Nav Pane')
+      }
+    }
   }
 </script>
+
+<style scoped>
+
+  .nav {
+    padding-right: 1rem;
+  }
+
+  .hamburgler-icon-wrapper {
+    position: absolute;
+    top: 1em;
+    right: 1em;
+    height: 26px;
+    width: 26px;
+    cursor: pointer
+  }
+
+  .hamburgler-icon,
+  .hamburgler-icon:before,
+  .hamburgler-icon:after {
+    content: '';
+    position: absolute;
+    border-radius: 1em;
+    height: 2px;
+    width: 26px;
+    background: black;
+    transition: all 0.2s ease
+  }
+
+  .hamburgler-icon {
+    top: 0.75em
+  }
+
+  .hamburgler-icon:before {
+    top: -0.55em
+  }
+
+  .hamburgler-icon:after {
+    top: 0.55em
+  }
+
+  .hamburgler-active .hamburgler-icon {
+    background: transparent;
+    transform: rotate(-135deg)
+  }
+
+  .hamburgler-active .hamburgler-icon:before,
+  .hamburgler-active .hamburgler-icon:after {
+    top: 0
+  }
+
+  .hamburgler-active .hamburgler-icon:before {
+    transform: rotate(90deg)
+  }
+
+  .hamburgler-active .hamburgler-icon,
+  .hamburgler-active .hamburgler-icon:before,
+  .hamburgler-active .hamburgler-icon:after {
+    background: white
+  }
+
+  .hamburgler-menu {
+    transition: all 0.4s ease;
+    opacity: 0;
+    pointer-events: none;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.9);
+    color: white;
+    text-align: center
+  }
+
+  .hamburgler-active .hamburgler-menu {
+    opacity: 1;
+    pointer-events: initial;
+  }
+
+  .hamburgler-menu-list {
+    display: block;
+    transition: all 0.4s ease;
+    padding: 0;
+    list-style-type: none;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) scale(1.2)
+  }
+
+  .hamburgler-active .hamburgler-menu-list {
+    transform: translate(-50%, -50%) scale(1)
+  }
+
+</style>

9f79c54958fd2e90c1e3da563fb26548073be74a
Michael Lobel <mglobel@gmail.com>
2017-02-22 12:05:27 -0600

    responsive google maps div

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -19,7 +19,7 @@
         </section>
       </div>
       <section class="contact-details col-sm-6">
-        <iframe width="500" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=1429%20Walnut%20Street%2C%20Suite%201300%20Philadelphia%2C%20PA%2019102&key=AIzaSyDyQpzdh030Eoxm4tfZ3afFJaXPYO8lTKU" allowfullscreen></iframe>
+        <div class='embed-container'><iframe width='500' height='450' frameborder='0' style='border:0' src='https://www.google.com/maps/embed/v1/place?q=1429%20Walnut%20Street%2C%20Suite%201300%20Philadelphia%2C%20PA%2019102&key=AIzaSyDyQpzdh030Eoxm4tfZ3afFJaXPYO8lTKU' allowfullscreen></iframe></div>
       
 
       </section>
@@ -37,4 +37,18 @@
   /* this should really just be a layout class */
   h1
     color: #42b983
+
+  .embed-container 
+    position: relative
+    padding-bottom: 56.25%
+    height: 0
+    overflow: hidden
+    max-width: 100%
+
+  .embed-container iframe, .embed-container object, .embed-container embed 
+    position: absolute
+    top: 0
+    left: 0
+    width: 100%
+    height: 100%
 </style>

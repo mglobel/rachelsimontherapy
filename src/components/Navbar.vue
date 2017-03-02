618ec9c743d1ee3e15e2975a4b0c8789936164ff
Michael Lobel <mglobel@gmail.com>
2017-03-02 00:01:33 -0500

    add rachel simon therapy nav header

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,12 +2,15 @@
   <nav class="navbar navbar-default">
     <!-- rainbow here -->
     <div class="container-fluid">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <router-link to="/" class="navbar-brand">Rachel Simon Therapy</router-linka>
+      </div>
       <div id="navbar" class="navbar-collapse collapse">
         <ul class="nav navbar-nav navbar-right">
           <li>
